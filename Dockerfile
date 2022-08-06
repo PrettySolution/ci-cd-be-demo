ffbd43829bdebfad423b7bfaa58bbe850986507a
VH🔥 <vherman@vf-soft.com>
2022-08-06 20:27:52 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:16.15.0 as build
+WORKDIR /usr/src/app
+COPY . .
+RUN npm i --production=false
+RUN npm run build
+
+FROM node:16.15.0-alpine
+ENV NODE_ENV=production
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm ci --only=production
+COPY --from=build /usr/src/app/ ./
+#COPY --from=build /usr/src/app/src/config/ ./config/
+CMD [ "npm", "run", "start" ]
+EXPOSE 3000
---
+++ index.ts
@@ -0,0 +1,34 @@
+import express, {Express, Request, Response} from 'express';
+import {networkInterfaces} from "os";
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/api/', (req: Request, res: Response) => {
+  res.send(`curl 'http://localhost:80/fibonacci?n=43'`);
+});
+
+app.get('/api/ip', (req, res) => {
+  const myIp = networkInterfaces()
+  res.send(myIp)
+})
+
+app.get('/api/health', (req: Request, res: Response) => {
+  res.status(200).send('healthy');
+});
+
+app.get('/api/fibonacci', (req: Request, res: Response) => {
+  console.log(req.query.n)
+  const f = fibonacci(req.query.n)
+  console.log(f)
+  res.send(`fibonacci = ${f}`);
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+
+function fibonacci(num: any): any {
+  if (num <= 1) return num;
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
